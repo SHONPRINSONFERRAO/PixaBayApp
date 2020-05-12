63aff7b9b196a62e326ba0c0b21e8d15668cba08
SHONPRINSONFERRAO <shonferrao@gmail.com>
2020-05-12 20:12:02 +0400

    Minor Fixes

--- app/src/main/java/com/apps/pixabayapp/ui/base/ViewModelFactory.kt
+++ app/src/main/java/com/apps/pixabayapp/ui/base/ViewModelFactory.kt
@@ -4,13 +4,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.apps.pixabayapp.data.api.ApiHelper
 import com.apps.pixabayapp.data.repository.PicsRepository
-import com.apps.pixabayapp.ui.details.viewmodel.DetailsViewModel
+import com.apps.pixabayapp.ui.home.viewmodel.HomeViewModel
 
 class ViewModelFactory(private val apiHelper: ApiHelper) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(DetailsViewModel::class.java)) {
-            return DetailsViewModel(PicsRepository(apiHelper)) as T
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            return HomeViewModel(PicsRepository(apiHelper)) as T
         }
         throw IllegalArgumentException("Unknown class name")
     }
